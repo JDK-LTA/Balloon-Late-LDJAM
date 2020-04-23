a26574c56ef82c2be574719806075c3ca390000f
Jota García García <jotagamedev@gmail.com>
2020-04-23 02:49:38 +0200

    Balloon move+Mobile touch support+Obstacle spawning+Wind particles

---
+++ Assets/Scripts/BalloonBehaviour.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonBehaviour : MonoBehaviour
+{
+    public Transform yPosition;
+    public float windSpeed = 2f;
+    public float size;
+
+    public float maxTime;
+    public float minSwipeDistance;
+
+    float swStartTime;
+    float swEndTime;
+    Vector3 swStartPos;
+    Vector3 swEndPos;
+    float swDistance;
+    float swTime;
+
+    public delegate void OnInput();
+    public event OnInput InputLeft;
+    public event OnInput InputRight;
+    public event OnInput InputDoubleTap;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+#if UNITY_STANDALONE || UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Input.mousePosition.x > Screen.width / 2)
+            {
+                InputRight?.Invoke();
+                RightToLeftWind();
+            }
+            else
+            {
+                InputLeft?.Invoke();
+                LeftToRightWind();
+            }
+            //Debug.Log(rb.velocity.x);
+        }
+#endif
+#if UNITY_IOS || UNITY_EDITOR
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.position.x > Screen.width / 2)
+            {
+                InputRight?.Invoke();
+                RightToLeftWind();
+            }
+            else
+            {
+                InputLeft?.Invoke();
+                LeftToRightWind();
+            }
+        }
+#endif
+    }
+
+    private void RightToLeftWind()
+    {
+        rb.AddForce(new Vector2(-windSpeed, 0), ForceMode2D.Force);
+    }
+    private void LeftToRightWind()
+    {
+        rb.AddForce(new Vector2(windSpeed, 0), ForceMode2D.Force);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Obstacle")
+        {
+            Debug.Log("Dead");
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    //HAY QUE SPAWNEARLOS DESDE EL CENTRO Y CREAR PREFABS QUE TENGAN COMO HIJOS LOS OBSTACULOS DE VERDAD
+    //PARA PODER CREAR DISTINTOS TIPOS DE OBSTACULOS CON DIFERENTES CONFIGURACIONES:        o    oo     <-UN PREFAB
+    //                                                                                      ooo   o     <-UN PREFAB
+    //                                                                                      o  o  o     <-UN PREFAB
+    public Transform spawnPoint;
+    public int difficulty = 1;
+    int maxDifficulty = 4;
+    List<List<GameObject>> allLevelObstacles = new List<List<GameObject>>();
+    public List<GameObject> diff1Obstacles;
+    public List<GameObject> diff2Obstacles;
+    public List<GameObject> diff3Obstacles;
+    public List<GameObject> diff4Obstacles;
+    [SerializeField] List<GameObject> obstacles = new List<GameObject>();
+    int lastObstacleIndex = -1;
+
+    public float cooldownToSpawn = 2f;
+    float t = 0;
+    public float cooldownBetweenLevels = 14f;
+    float l = 0;
+
+    private void Start()
+    {
+        allLevelObstacles.Add(diff1Obstacles);
+        allLevelObstacles.Add(diff2Obstacles);
+        allLevelObstacles.Add(diff3Obstacles);
+        allLevelObstacles.Add(diff4Obstacles);
+
+        for (int i = 0; i < difficulty; i++)
+        {
+            obstacles.AddRange(allLevelObstacles[i]);
+        }
+    }
+    private void Update()
+    {
+        t += Time.deltaTime;
+        l += Time.deltaTime;
+        if (t >= cooldownToSpawn)
+        {
+            t = 0;
+            SpawnObstacle();
+        }
+        if (l >= cooldownBetweenLevels && difficulty < maxDifficulty)
+        {
+            l = 0;
+            UpTheDifficulty();
+        }
+    }
+
+    private void SpawnObstacle()
+    {
+        int index;
+        do
+        {
+            index = Random.Range(0, obstacles.Count);
+        } while (index == lastObstacleIndex);
+        lastObstacleIndex = index;
+
+        GameObject go = Instantiate(obstacles[index], spawnPoint.position, spawnPoint.rotation);
+        //if (Random.Range(0, 2) == 1)
+        //{
+        //    Vector3 ls = go.transform.localScale;
+        //    ls.x *= -1;
+        //    go.transform.localScale = ls;
+        //}
+    }
+    private void UpTheDifficulty()
+    {
+        difficulty++;
+        obstacles.AddRange(allLevelObstacles[difficulty - 1]);
+    }
+    private void DownTheDifficulty()
+    {
+        obstacles = obstacles.Except(allLevelObstacles[difficulty - 1]).ToList();
+        difficulty--;
+    }
+}
